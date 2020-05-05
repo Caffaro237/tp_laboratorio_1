ce9895090a493712ec0826716779329db190c948
unknown <faundodarian@hotmail.com>
2020-05-05 17:17:00 -0300

    Trabajo Desaprobado

--- Trabajo Practico 1 - Rehecho/resetear_flags.c
+++
@@ -1,9 +0,0 @@
-void resetear_flags(int* p) //Funcion para resetear las banderas
-{
-    /*
-        Le llega la posicion de menoria donde estan
-        las banderas y las pone en 0 nuevamente
-    */
-
-    *p = 0;
-}
