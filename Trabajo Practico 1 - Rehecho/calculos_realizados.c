042bc8fb0f8a8f4651cc8a60cb17662c1999d9bb
unknown <faundodarian@hotmail.com>
2020-05-05 17:10:27 -0300

    Trabajo Rehecho

---
+++ Trabajo Practico 1 - Rehecho/calculos_realizados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prototipos.h"
+
+void calculos_realizados() //Funcion para mostrar el mensaje de calculos realizados
+{
+    printf("a) Calculo de la suma\n");
+    printf("b) Calculo de la resta\n");
+    printf("c) Calculo de la division\n");
+    printf("d) Calculo de la multiplicacion\n");
+    printf("e) Calculo del factorial\n\n");
+
+    printf("     ***Realizados correctamente***\n");
+    printf("***Listo para informar los resultados***\n\n");
+}
---
+++ Trabajo Practico 1 - Rehecho/resetear_flags.c
@@ -0,0 +1,9 @@
+void resetear_flags(int* p) //Funcion para resetear las banderas
+{
+    /*
+        Le llega la posicion de menoria donde estan
+        las banderas y las pone en 0 nuevamente
+    */
+
+    *p = 0;
+}
